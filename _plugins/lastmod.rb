2695d9cc26e987d0de0d634647816be50eabafbf
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-05-04 04:56:55 +0900

    Update lastmod.rb

--- _plugins/lastmod.rb
+++ _plugins/lastmod.rb
@@ -3,6 +3,7 @@ module Jekyll
   class LastModifiedDateTag < Liquid::Tag
     def initialize(tag_name, text, tokens)
       super
+      @format = text.strip # Save the provided date format
     end
 
     def render(context)
@@ -18,11 +19,16 @@ module Jekyll
         modified_date = File.mtime(file_path)
       end
 
-      # Format the last modified date and return
-      modified_date.strftime("%Y-%m-%d %H:%M:%S")
+      # If no format is provided, use default format
+      if @format.nil? || @format.empty?
+        @format = "%Y-%m-%d %H:%M:%S"
+      end
+
+      # Format the date according to the provided date format
+      modified_date.strftime(@format)
     end
   end
 end
 
 # Register the Liquid tag for last modified date
-Liquid::Template.register_tag('modified_date', Jekyll::LastModifiedDateTag)
+# Liquid::Template.register_tag('modified_date', Jekyll::LastModifiedDateTag)
