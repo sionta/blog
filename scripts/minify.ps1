1b2d1484d7721e3600f190cdbec3cfdcc690796d
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-07-02 03:46:27 +0900

    Update minify.ps1

--- scripts/minify.ps1
+++ scripts/minify.ps1
@@ -4,22 +4,21 @@ param (
     [Alias('s')][string]$Source
 )
 
-
 if (Test-Path "../_config.yml" -PathType Leaf) {
     Set-Location ".."
 }
 
-if (-not(Test-Path "node_modules/.bin/*")) {
+if (-not(Test-Path "node_modules/.bin/*") -or ($ENV:JEKYLL_ENV -eq 'production')) {
     npm install
 }
 
 $BUILD_DIR = Resolve-Path($Source)
 
 # Minify HTML files
-html-minifier --input-dir "$BUILD_DIR" --output-dir "$BUILD_DIR" --file-ext html --collapse-whitespace --remove-comments --remove-attribute-quotes --minify-css --minify-js
+& "node_modules/.bin/html-minifier" --input-dir "$BUILD_DIR" --output-dir "$BUILD_DIR" --file-ext html --collapse-whitespace --remove-comments --remove-attribute-quotes --minify-css --minify-js
 
 # Minify CSS files
-cleancss -o "$BUILD_DIR/assets/css/style.css" "$BUILD_DIR/assets/css/style.css"
+& "node_modules/.bin/cleancss" -o "$BUILD_DIR/assets/css/style.css" "$BUILD_DIR/assets/css/style.css"
 
 # Minify JS files
-uglifyjs -o "$BUILD_DIR/assets/js/main.js" "$BUILD_DIR/assets/js/main.js"
+& "node_modules/.bin/uglifyjs" -o "$BUILD_DIR/assets/js/main.js" "$BUILD_DIR/assets/js/main.js"
