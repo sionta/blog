9eebb79ed8f1ae9886a6dd7e75b01bb47626ad86
Andre Attamimi <73837775+sionta@users.noreply.github.com>
2024-08-06 13:47:07 +0900

    ads

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,5 @@
 (function () {
-  const gotoTop = document.getElementById("goto-top");
-  if (gotoTop) {
-    window.addEventListener("scroll", () => {
-      gotoTop.classList.toggle("active", window.scrollY > 350);
-    });
-    gotoTop.addEventListener("click", () => {
-      window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-  }
-
+  // document.addEventListener("DOMContentLoaded", () => {
   if (window.location.pathname !== "/blog/") {
     document.addEventListener("keydown", function (event) {
       if (event.ctrlKey && event.shiftKey && event.key === "F") {
@@ -17,82 +8,93 @@
     });
   }
 
-  const articleBody = "[itemprop=articleBody]";
-  const articleHeadings = "h2, h3";
+  const gotoTop = document.getElementById("goto-top");
+  if (gotoTop) {
+    window.addEventListener("scroll", () => {
+      gotoTop.classList.toggle("active", window.scrollY > 350);
+    });
+    gotoTop.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
 
-  if (window.anchors) {
-    anchors.options = {
-      placement: "right",
-      visible: "always",
-      icon: "#",
-      class: "anchor-link",
-    };
-    anchors
-      .add(`${articleBody} :where(${articleHeadings})`)
-      .remove(".no-anchor, .no_anchor");
-  }
+    const articleBody = "[itemprop=articleBody]";
+    const articleHeadings = "h2, h3";
 
-  if (window.tocbot) {
-    const initializeToc = () => {
-      const tocTag = window.innerWidth >= 768 ? "#toc" : "#toc-mobile";
-      if (!tocTag) return;
+    if (window.anchors) {
+      anchors.options = {
+        placement: "right",
+        visible: "always",
+        icon: "#",
+        class: "anchor-link",
+      };
+      anchors
+        .add(`${articleBody} :where(${articleHeadings})`)
+        .remove(".no-anchor, .no_anchor");
+    }
 
-      const config = {
-        tocSelector: tocTag,
-        contentSelector: articleBody,
-        ignoreSelector: ".no_toc, .no-toc",
-        headingSelector: articleHeadings,
-        orderedList: false,
-        collapseDepth: 0,
-        listClass: "toc-list",
-        listItemClass: "toc-item",
-        activeListItemClass: "is-active-item",
-        linkClass: "toc-link",
-        activeLinkClass: "is-active-link",
-        isCollapsedClass: "is-collapsed",
-        collapsibleClass: "is-collapsible",
-        positionFixedClass: "is-position-fixed",
-        fixedSidebarOffset: "auto",
+    if (window.tocbot) {
+      const initializeToc = () => {
+        const tocTag = window.innerWidth >= 768 ? "#toc" : "#toc-mobile";
+        if (!tocTag) return;
+
+        const config = {
+          tocSelector: tocTag,
+          contentSelector: articleBody,
+          ignoreSelector: ".no_toc, .no-toc",
+          headingSelector: articleHeadings,
+          orderedList: false,
+          collapseDepth: 0,
+          listClass: "toc-list",
+          listItemClass: "toc-item",
+          activeListItemClass: "is-active-item",
+          linkClass: "toc-link",
+          activeLinkClass: "is-active-link",
+          isCollapsedClass: "is-collapsed",
+          collapsibleClass: "is-collapsible",
+          positionFixedClass: "is-position-fixed",
+          fixedSidebarOffset: "auto",
+        };
+
+        if (tocTag == "#toc-mobile") {
+          config.orderedList = true;
+          config.headingSelector = "h2";
+        }
+
+        tocbot.destroy();
+        tocbot.init(config);
+        tocbot.refresh();
       };
 
-      if (tocTag == "#toc-mobile") {
-        config.orderedList = true;
-        config.headingSelector = "h2";
-      }
+      initializeToc();
+      window.addEventListener("resize", initializeToc);
+    }
 
-      tocbot.destroy();
-      tocbot.init(config);
-      tocbot.refresh();
-    };
+    if (window.mermaid) {
+      const initializeMermaid = () => {
+        const mermaidTag = document.querySelectorAll(".language-mermaid");
+        if (!mermaidTag) return;
 
-    initializeToc();
-    window.addEventListener("resize", initializeToc);
+        mermaidTag.forEach((block) => {
+          const pre = block.parentElement; // pre
+          const a = document.createElement("figure");
+          const b = document.createElement("pre");
+          a.classList.add("diagram");
+          b.classList.add("mermaid");
+          b.innerHTML = pre.innerText;
+          a.appendChild(b);
+          pre.replaceWith(a);
+        });
+
+        mermaid.initialize({
+          startOnLoad: true,
+          theme:
+            THEME.mermaid[localStorage.getItem("theme") || THEME.mermaid.light],
+          logLevel: "warn",
+        });
+      };
+
+      initializeMermaid();
+    }
   }
-
-  if (window.mermaid) {
-    const initializeMermaid = () => {
-      const mermaidTag = document.querySelectorAll(".language-mermaid");
-      if (!mermaidTag) return;
-
-      mermaidTag.forEach((block) => {
-        const pre = block.parentElement; // pre
-        const a = document.createElement("figure");
-        const b = document.createElement("pre");
-        a.classList.add("diagram");
-        b.classList.add("mermaid");
-        b.innerHTML = pre.innerText;
-        a.appendChild(b);
-        pre.replaceWith(a);
-      });
-
-      mermaid.initialize({
-        startOnLoad: true,
-        theme:
-          THEME.mermaid[localStorage.getItem("theme") || THEME.mermaid.light],
-        logLevel: "warn",
-      });
-    };
-
-    initializeMermaid();
-  }
+  // });
 })();
